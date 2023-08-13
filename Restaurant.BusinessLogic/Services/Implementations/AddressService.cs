54348265978d7a537f20a74e1b4a4ca916ec9846
Dairo Timileyin <timileyin4baze@gmail.com>
2023-08-13 23:11:36 +0100

    Creating Response DTO

--- Restaurant.BusinessLogic/Services/Implementations/AddressService.cs
+++ Restaurant.BusinessLogic/Services/Implementations/AddressService.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Restaurant.Data.Entities;
+using Restaurant.Data.Repository.Interface;
+using Restaurant.DTO;
+using Restaurant.DTO.Request;
+using Restaurant.DTO.Response;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +14,50 @@ namespace Restaurant.BusinessLogic.Services.Implementations
 {
     public class AddressService
     {
-        public AddressService()
+        private readonly UserManager<Customer> _userManager;
+        private readonly IGenericRepo<Address> _genericRepoAddress;
+        public AddressService(UserManager<Customer> userManager, IGenericRepo<Address> genericRepoAddress)
         {
+            _userManager = userManager;
+            _genericRepoAddress = genericRepoAddress;
+        }
+
+        public async Task<GenericResponse<AddingAddressResponseDTO>> AddProductToCartAsync(AddingAddressRequestDTO addingAddressRequestDTO)
+        {
+            var user = await _userManager.FindByIdAsync(addingAddressRequestDTO.CustomerId.ToString());
+
+            Address address = new()
+            {
+                Street = addingAddressRequestDTO.Street,
+                City = addingAddressRequestDTO.City,
+                State = addingAddressRequestDTO.State,
+                PostalCode = addingAddressRequestDTO.PostalCode,
+                Country = addingAddressRequestDTO.Country,
+                CustomerId = addingAddressRequestDTO.CustomerId,
+                IsShippingAddress = addingAddressRequestDTO.IsShippingAddress
+            };
+
+            bool success = await _genericRepoAddress.InsertAsync(address);
+
+            if (success)
+            {
+                var addingAddressResponseDTO = new AddingAddressResponseDTO()
+                {
+                    Id = address.Id,
+                    Street = addingAddressRequestDTO.Street,
+                    City = addingAddressRequestDTO.City,
+                    State = addingAddressRequestDTO.State,
+                    PostalCode = addingAddressRequestDTO.PostalCode,
+                    Country = addingAddressRequestDTO.Country,
+                    CustomerId = addingAddressRequestDTO.CustomerId,
+                    IsShippingAddress = addingAddressRequestDTO.IsShippingAddress
+
+                };
+
+                return GenericResponse<AddingAddressResponseDTO>.SuccessResponse(addingAddressResponseDTO, "Address Added successfully");
+            }
+
+            return GenericResponse<AddingAddressResponseDTO>.ErrorResponse("Error, Can't add Address", false);
         }
     }
 }
