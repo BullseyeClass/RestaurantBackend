e6ee5502f569c34a09f1ffe7054e03f59729947f
Ayilara Richard <Ayilara.R@Bullseyemanpower.com>
2023-08-13 21:23:45 +0100

    added customer support dto, snd customer supportservice

---
+++ Restaurant.DTO/Request/RequestingCustomerSupportDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.DTO.Request
+{
+    public class RequestingCustomerSupportDTO
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Message { get; set; }
+        public string CustomerId { get; set; }
+    }
+}
