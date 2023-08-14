6962be2f6357c6a255f7a59be0e0b6a7ec27ab64
Ayilara Richard <Ayilara.R@Bullseyemanpower.com>
2023-08-14 08:49:51 +0100

    added required on the support dto

--- Restaurant.DTO/Request/RequestingCustomerSupportDTO.cs
+++ Restaurant.DTO/Request/RequestingCustomerSupportDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace Restaurant.DTO.Request
     public class RequestingCustomerSupportDTO
     {
         public Guid Id { get; set; } = Guid.NewGuid();
+        [Required(ErrorMessage = "Message Field is required")]
         public string Message { get; set; }
         public string CustomerId { get; set; }
     }
