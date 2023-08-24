5734751eea4a7ccd2d046187699b94ddb374896c
Dairo Timileyin <timileyin4baze@gmail.com>
2023-08-24 10:39:44 +0100

    Created Order Endpoint

---
+++ Restaurant.DTO/Response/GetAllOrderResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿using Restaurant.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.DTO.Response
+{
+    public class GetAllOrderResponseDTO
+    {
+        public Guid Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public virtual Guid CustomerId { get; set; }
+    }
+}
